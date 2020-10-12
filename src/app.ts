759c7a591ac34159cd5fb94a86b6773b0476e13e
Vitalii <gotbubu0@gmail.com>
2020-10-12 20:01:12 +0300

    - in process

--- src/app.ts
+++ src/app.ts
@@ -6,31 +6,46 @@ import * as helmet from 'helmet';
 import * as dotenv from 'dotenv';
 import * as morgan from 'morgan';
 import * as path from 'path';
-
+import {NextFunction} from 'express';
 
 dotenv.config({});
 
 // Обмеження на кількість запитів за одиницю часу
 const serverRequestLimit = rateLimit({
-    windowMs: 10000,
-    max: 100  // TODO ad this to .dotenv
-})
+  windowMs: 10000,
+  max: 100 // TODO ad this to .dotenv
+});
 
 class App {
     public readonly app: express.Application = express();
 
     constructor() {
-        (global as any).appRoot = path.resolve(process.cwd(), '../')
-        // Logger
-        this.app.use(morgan('dev'));
-        // Захист
-        this.app.use(helmet());
-        this.app.use(serverRequestLimit);
-        this.app.use(cors({}));
+      (global as any).appRoot = path.resolve(process.cwd(), '../');
+      // Logger
+      this.app.use(morgan('dev'));
+      // Захист
+      this.app.use(helmet());
+      this.app.use(serverRequestLimit);
+      this.app.use(cors({}));
 
-        this.app.use(express.json());
-        this.app.use(express.urlencoded({extended: true}));
+      this.app.use(express.json());
+      this.app.use(express.urlencoded({extended: true}));
 
-        this.app.use(express.static(path.join((global as any), 'public')));
+      this.app.use(express.static(path.join((global as any), 'public')));
+
+      // TODO router
+
+      this.app.use(this.customErrorHandler);
+    }
+
+    private customErrorHandler(err: any, req: Request, res: Response, next: NextFunction) {
+      res
+        .status(err.status || 500)
+        .json({
+          message: err.message || 'Error unknown',
+          code: err.code
+        });
     }
 }
+
+export const app = new App().app;
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,18 @@
 import * as http from 'http';
+import {app} from './app';
 
-let server = http.createServer(app);
+const server = http.createServer(app);
 
-server.listen(5000);
+server.listen(5000, () => {
+  console.log('Listen 5000');
+});
+
+process.on('SIGTERM', () => {
+  server.close(() => {
+    process.exit(0);
+  });
+});
+
+process.on('unhandledRejection', error => {
+  console.log(error);
+});
